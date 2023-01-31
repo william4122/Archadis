83a4832246f12c844e712c9431969985e9021fc5
william4122 <119632767+william4122@users.noreply.github.com>
2023-01-30 20:07:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const logs = getLogEntries(); 
-logs.map(log=>{
+logs.map(log => {
    let logSections = log.split("][");
    let timestamp = logSections[0];
    // do something with the timestamp, such as converting it to local time
+});
+
 document.addEventListener("DOMContentLoaded", function() {
   convertTimestampsOnHover();
 });
+
 let file = document.getElementById("file-upload").files[0];
 let reader = new FileReader();
 reader.onload = function() {
@@ -14,6 +17,7 @@ reader.onload = function() {
   document.getElementById("logs").innerHTML = logText;
   convertTimestampsOnHover();
 };
+
 document.getElementById("file-upload").addEventListener("change", function() {
   let file = this.files[0];
   let reader = new FileReader();
@@ -25,18 +29,20 @@ document.getElementById("file-upload").addEventListener("change", function() {
   };
   reader.readAsText(file);
 });
+
 function convertTimestamps(logText) {
   // Create a regular expression to match timestamps in the format "YYYY-MM-DD HH:mm:ss UTC"
   let regex = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC/g;
   // Replace all occurrences of the timestamp with a span element with the "timestamp" class
   return logText.replace(regex, `<span class="timestamp">$&</span>`);
 }
+
 function convertTimestampsOnHover() {
   let timestampElements = document.getElementsByClassName("timestamp");
   for (let i = 0; i < timestampElements.length; i++) {
     let timestampElement = timestampElements[i];
+    let utcTimestamp = timestampElement.textContent;
     timestampElement.onmouseover = function() {
-      let utcTimestamp = timestampElement.textContent;
       let localTimestamp = new Date(utcTimestamp);
       localTimestamp = localTimestamp.toLocaleString();
       timestampElement.textContent = localTimestamp;
@@ -44,7 +50,11 @@ function convertTimestampsOnHover() {
     timestampElement.onmouseout = function() {
       timestampElement.textContent = utcTimestamp;
     }
-   // do something with the other sections, such as highlighting keywords
+  }
+}
+
+// do something with the other sections, such as highlighting keywords
+
 let input = document.getElementById("file-upload");
 input.addEventListener("change", function(event) {
   let file = event.target.files[0];
@@ -55,26 +65,25 @@ input.addEventListener("change", function(event) {
     let logDangerKeywords = ["ERR", "Could not fetch jobs: Invalid server response: 404", "fail", "Service stop signal received.", "Cannot save RTS data: unable to open database file", "Received stop signal from service controller", "Cannot finish fetching user accounts:"];
     let logGoodKeywords = ["success", "Results sent, scan is complete", "completed", "Agent has started", "Found matches!", "Yara scan completed." ];
     let logInfoKeywords = ["INF", "Waiting for remote API to approve upload", "status", "Starting as windows service"];
-
-    // Apply the highlightKeywords function to each keyword list
-    logText = highlightKeywords(logText, logDangerKeywords, "logdanger");
-    logText = highlightKeywords(logText, logGoodKeywords, "loggood");
-    logText = highlightKeywords(logText, logInfoKeywords, "loginfo");
-
-    // Display the log file with the keywords highlighted in the appropriate class
-    let logsElement = document.getElementById("logs");
-    logsElement.innerHTML = logText;
-  };
-  reader.readAsText(file);
+// Apply the highlightKeywords function to each keyword list
+logText = highlightKeywords(logText, logDangerKeywords, "logdanger");
+logText = highlightKeywords(logText, logGoodKeywords, "loggood");
+logText = highlightKeywords(logText, logInfoKeywords, "loginfo");
+// Display the log file with the keywords highlighted in the appropriate class
+let logsElement = document.getElementById("logs");
+logsElement.innerHTML = logText;
+};
+reader.readAsText(file);
 });
 
 function highlightKeywords(logText, keywords, className) {
-  // Iterate through the list of keywords
-  for (let i = 0; i < keywords.length; i++) {
-    let keyword = keywords[i];
-    // Create a regular expression with the keyword and the global flag
-    let regex = new RegExp(keyword, "g");
-    // Replace all occurrences of the keyword with a span element with the className class
-    logText = logText.replace(regex, `<span class="${className}">${keyword}</span>`);
-  }
-  return logText;
+// Iterate through the list of keywords
+for (let i = 0; i < keywords.length; i++) {
+let keyword = keywords[i];
+// Create a regular expression with the keyword and the global flag
+let regex = new RegExp(keyword, "g");
+// Replace all occurrences of the keyword with a span element with the className class
+logText = logText.replace(regex, <span class="${className}">${keyword}</span>);
+}
+return logText;
+}
