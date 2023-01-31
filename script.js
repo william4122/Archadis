206773458786b69b35b81271d8d0cc48b7284e41
william4122 <119632767+william4122@users.noreply.github.com>
2023-01-30 19:20:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,8 @@
-let input = document.getElementById("file-upload");
-input.addEventListener("change", function(event) {
-  let file = event.target.files[0];
-  let reader = new FileReader();
-  reader.onload = function(event) {
-    let logText = event.target.result;
-    // Create a list of keywords for each class
-    let logDangerKeywords = ["ERR", "Could not fetch jobs: Invalid server response: 404", "fail", "Service stop signal received.", "Cannot save RTS data: unable to open database file", "Received stop signal from service controller", "Cannot finish fetching user accounts:"];
-    let logGoodKeywords = ["success", "Results sent, scan is complete", "completed", "Agent has started", "Found matches!", "Yara scan completed." ];
-    let logInfoKeywords = ["INF", "Waiting for remote API to approve upload", "status", "Starting as windows service"];
-
-    // Apply the highlightKeywords function to each keyword list
-    logText = highlightKeywords(logText, logDangerKeywords, "logdanger");
-    logText = highlightKeywords(logText, logGoodKeywords, "loggood");
-    logText = highlightKeywords(logText, logInfoKeywords, "loginfo");
-
-    // Display the log file with the keywords highlighted in the appropriate class
-    let logsElement = document.getElementById("logs");
-    logsElement.innerHTML = logText;
-  };
-  reader.readAsText(file);
-});
-
-function highlightKeywords(logText, keywords, className) {
-  // Iterate through the list of keywords
-  for (let i = 0; i < keywords.length; i++) {
-    let keyword = keywords[i];
-    // Create a regular expression with the keyword and the global flag
-    let regex = new RegExp(keyword, "g");
-    // Replace all occurrences of the keyword with a span element with the className class
-    logText = logText.replace(regex, `<span class="${className}">${keyword}</span>`);
-  }
-  return logText;
-}
+const logs = getLogEntries(); 
+logs.map(log=>{
+   let logSections = log.split("][");
+   let timestamp = logSections[0];
+   // do something with the timestamp, such as converting it to local time
 document.addEventListener("DOMContentLoaded", function() {
   convertTimestampsOnHover();
 });
@@ -73,5 +44,39 @@ function convertTimestampsOnHover() {
     timestampElement.onmouseout = function() {
       timestampElement.textContent = utcTimestamp;
     }
+   // do something with the other sections, such as highlighting keywords
+let input = document.getElementById("file-upload");
+input.addEventListener("change", function(event) {
+  let file = event.target.files[0];
+  let reader = new FileReader();
+  reader.onload = function(event) {
+    let logText = event.target.result;
+    // Create a list of keywords for each class
+    let logDangerKeywords = ["ERR", "Could not fetch jobs: Invalid server response: 404", "fail", "Service stop signal received.", "Cannot save RTS data: unable to open database file", "Received stop signal from service controller", "Cannot finish fetching user accounts:"];
+    let logGoodKeywords = ["success", "Results sent, scan is complete", "completed", "Agent has started", "Found matches!", "Yara scan completed." ];
+    let logInfoKeywords = ["INF", "Waiting for remote API to approve upload", "status", "Starting as windows service"];
+
+    // Apply the highlightKeywords function to each keyword list
+    logText = highlightKeywords(logText, logDangerKeywords, "logdanger");
+    logText = highlightKeywords(logText, logGoodKeywords, "loggood");
+    logText = highlightKeywords(logText, logInfoKeywords, "loginfo");
+
+    // Display the log file with the keywords highlighted in the appropriate class
+    let logsElement = document.getElementById("logs");
+    logsElement.innerHTML = logText;
+  };
+  reader.readAsText(file);
+});
+
+function highlightKeywords(logText, keywords, className) {
+  // Iterate through the list of keywords
+  for (let i = 0; i < keywords.length; i++) {
+    let keyword = keywords[i];
+    // Create a regular expression with the keyword and the global flag
+    let regex = new RegExp(keyword, "g");
+    // Replace all occurrences of the keyword with a span element with the className class
+    logText = logText.replace(regex, `<span class="${className}">${keyword}</span>`);
   }
+  return logText;
 }
+
